7a90f2ac9174c366b88849e4629dc500413b94e5
Mm <maxkulezenin@gmail.com>
2026-03-17 22:39:29 +0700

    base enemy Behavior

---
+++ Assets/Scripts/EnemyModule/EnemyConfig.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace EnemyModule
+{
+    [CreateAssetMenu(menuName = "ScriptableObjects/EnemyConfig", fileName = "EnemyConfig")]
+    public class EnemyConfig : ScriptableObject 
+    {
+        [field: SerializeField]
+        public int Health { get; private set; }
+        
+        [field: SerializeField]
+        public int Damage { get; private set; }
+        
+        [field: SerializeField]
+        public int AttackCooldownSec { get; private set; }
+        
+        [field: SerializeField]
+        public int MoveSpeed { get; private set; }
+    }
+}
