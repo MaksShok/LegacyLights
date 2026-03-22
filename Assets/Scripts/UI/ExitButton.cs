83c14693b73d120705bb97c5e762b55fcdc89624
Mm <maxkulezenin@gmail.com>
2026-03-22 23:05:15 +0700

    TMP Import and ExitButton

---
+++ Assets/Scripts/UI/ExitButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace UI
+{
+    public class ExitButton : MonoBehaviour
+    {
+        public void ExitGame()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        
+            Debug.Log("Приложение закрывается");
+        }
+    }
+}
